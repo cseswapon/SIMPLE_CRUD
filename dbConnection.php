1467f1fd23b36a3e42f5e737923f6e39d84a4840
cseswapon <cseswaponsaha@gmail.com>
2024-12-03 20:18:30 +0600

    [ADD] - added first commit

---
+++ dbConnection.php
@@ -0,0 +1,7 @@
+<?php
+    $databaseHost = 'localhost';
+    $databaseName = 'test';
+    $databaseUsername = 'root';
+    $databasePassword = 'root';
+    $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
